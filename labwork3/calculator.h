0f0c3ff0001d2588a4d07f0161a3ab1b61b19a98
grazar <grazar12345@yandex.ua>
2019-12-13 22:42:38 +0200

    3laba

---
+++ labwork3/calculator.h
@@ -0,0 +1,20 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+<<<<<<< HEAD
+	public:
+		int Add (double, double);
+		int Sub (double, double);
+=======
+    public:
+        int Add (int, int);
+        int Mul (double, double);
+>>>>>>> e0ea21b... add a multiplication operation
+	Mul = double * double;
+return something;
+postav'te A :)
+};
+
+#endif//CALCULATOR_H
